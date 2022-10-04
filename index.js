d5a224c16f33ce4829276d5d80d72e14075c5be7
lina-22 <haquefahinur@gmail.com>
2022-10-04 23:33:35 +0200

    jQuery implement

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // document.querySelector(h1).innerHTML="hello" 
 // or
-$("h1").text("Hello jQuery little world");
+// $("h1").text("Hello jQuery little world");
+
+$(".my-div h1").text("hi heading");
+
+$("#p1,h1").text("hello p or h1 how are you?");
